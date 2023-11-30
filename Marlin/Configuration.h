9ac46d2fa9b74644a4244678da271e521e43a9cf
JohnTheCoolingFan <ivan8215145640@gmail.com>
2023-11-30 09:10:31 +0300

    Disable endstop plugs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1034,9 +1034,9 @@
 // Specify here all the endstop connectors that are connected to any endstop or probe.
 // Almost all printers will be using one per axis. Probes will use one or more of the
 // extra connectors. Leave undefined any used for non-endstop and non-probe purposes.
-#define USE_XMIN_PLUG
-#define USE_YMIN_PLUG
-#define USE_ZMIN_PLUG
+//#define USE_XMIN_PLUG
+//#define USE_YMIN_PLUG
+//#define USE_ZMIN_PLUG
 //#define USE_IMIN_PLUG
 //#define USE_JMIN_PLUG
 //#define USE_KMIN_PLUG
